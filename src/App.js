23fad5c02b5772d4ed552e5881382454c6060b9f
begli.amanov <beglee.amanov@gmail.com>
2026-04-25 22:35:16 +0300

    Add open ai to project

--- src/App.js
+++ src/App.js
@@ -2,10 +2,18 @@ import { InferenceClient } from '@huggingface/inference';
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
+import { OpenAI } from "openai";
 
-const hf = new InferenceClient('ACCESS_TOKEN');
+
+const client = new OpenAI({
+	baseURL: "https://router.huggingface.co/v1",
+	apiKey: 'API_KEY',
+  dangerouslyAllowBrowser: true
+});
+
+
 export default function Hface () {
-  const [input, setInput] = useState('hello');
+  const [input, setInput] = useState('');
   const [output, setOutput] = useState('');
   const [loading, setLoading] = useState(false);
   async function handleAsk() {
@@ -15,11 +23,17 @@ export default function Hface () {
     setLoading(true);
     setOutput('');
     try {
-      const result = await hf.textGeneration({
-    model: "mistralai/Mistral-7B-Instruct-v0.3",
-    inputs: "Artificial intelligence is"
+      const result =await client.chat.completions.create({
+	model: "meta-llama/Llama-3.1-8B-Instruct:cerebras",
+    messages: [
+        {
+            role: "user",
+            content: input,
+        },
+    ],
 })
-      setOutput(result.generated_text);
+      setOutput(result.choices[0].message.content);
+      console.log(result.choices[0].message.content)
     }catch(err){
       setOutput('Error: '+ err.message)
       console.log(err)
